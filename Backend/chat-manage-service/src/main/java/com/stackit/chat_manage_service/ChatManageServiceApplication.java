e6de9c1525876718cc410e7b91e0acbf79b6291e
Dark Techie <vanditkalathiya18@gmail.com>
2025-07-12 08:50:56 +0530

    first commit

---
+++ Backend/chat-manage-service/src/main/java/com/stackit/chat_manage_service/ChatManageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackit.chat_manage_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatManageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatManageServiceApplication.class, args);
+	}
+
+}
---
+++ Backend/chat-manage-service/src/test/java/com/stackit/chat_manage_service/ChatManageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackit.chat_manage_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatManageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
